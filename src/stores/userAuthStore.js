477235998d3a997729da274fab40fb099fd941d6
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-23 21:21:43 +0800

    Configure zustand store of user auth

---
+++ src/stores/userAuthStore.js
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+export function userAuthStore = create((set) => ({
+	username: null,
+	email: null,
+	password: null,
+	isAuthenticated: false,
+
+	setUsername: (username) => ({ username: username }),
+	setEmail: (email) => ({ email: email }),
+	setPassword: (password) => ({ password: password }),
+	setIsAuthenticated: (status) => ({ isAuthenticated: status })
+}))
