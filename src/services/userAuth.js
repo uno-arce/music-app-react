5f8e02e1f5b6b4b7c29068283f869b6a2064416a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-23 21:19:29 +0800

    Added user auth api calls of login and register

--- src/services/userAuth.js
+++ src/services/userAuth.js
@@ -1,1 +1,26 @@
-userAuth.js
+import instance from './instance'
+
+export default userAuth = {
+	login: (email, password) => {
+		instance.post('/login', {
+			email: email,
+			password: password
+		}).then(response => {
+			// save token and user data to user zustand store
+		}).catch(error => {
+			console.log('Login call error: ' + error)
+		})
+	},
+
+	register: (username, email, password) => {
+		instance.post('/register', {
+			username: username,
+			email: email,
+			password: password
+		}).then(response => {
+			return response.status
+		}).catch(error => {
+			console.log('Register call error: ' + error)
+		})
+	}
+}
