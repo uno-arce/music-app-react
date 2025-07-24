fd0cae8f2498a26174ba19faef222a70dde578dd
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-24 23:47:38 +0800

    Started a reusable form component incomplete

---
+++ src/components/form.jsx
@@ -0,0 +1,20 @@
+
+export default function Form({ input, call, store }) {
+
+	const {
+		username,
+		email,
+		password,
+		isAuthenticated,
+
+		setUsername,
+		setEmail,
+		setPassword,
+		setIsAuthenticated
+	} = store
+
+	const formType = () => {
+		
+	}
+
+}
