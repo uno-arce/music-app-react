157013460bb730d1fbf93f7526b2247260641bc6
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-27 19:00:13 +0800

    Setup spotify api service

--- src/services/spotifyApi.js
+++ src/services/spotifyApi.js
@@ -1,1 +1,58 @@
-spotifyApi.js
+import instance from './instance'
+
+const spotifyAPI = {
+	getSavedTracks: () => {
+		instance.get('/saved-tracks')
+		.then(response  => {
+			return response
+		})
+		.catch(error => {
+			console.log(error)
+			return error
+		})
+	},
+
+	getUserPlaylists: () => {
+		instance.get('/playlists')
+		.then(response => {
+			return response
+		})
+		.catch(error => {
+			console.log(error)
+			return error
+		})
+	},
+
+	getRecentlyPlayed: () => {
+		instance.get('/recently-played')
+		.then(response => {
+			return response
+		})
+		.catch(error => {
+			console.log(error)
+			return error
+		})
+	},
+
+	getMostlyPlayed: () => {
+		instance.get('/mostly-played')
+		.then(response => {
+			return response
+		})
+		.catch(error => {
+			console.log(error)
+			return error
+		})
+	},
+
+	getMostlyListened: () => {
+		instance.get('/mostly-listened')
+		.then(response => {
+			return response
+		})
+		.catch(error => {
+			console.log(error)
+			return error
+		})
+	}
+}
