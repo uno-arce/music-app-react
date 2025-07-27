d7ee8eead79168909f17a9ce6ae0bd71e9743d2d
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-27 19:00:59 +0800

    Setup spotify authorization service

--- src/services/spotifyAuth.js
+++ src/services/spotifyAuth.js
@@ -1,1 +1,36 @@
-spotifyAuth.js
+import instance from './instance'
+
+const spotifyAuth = {
+	requestAuthorization: () => {
+		instance.get('/')
+		.then(response => {
+			return response
+		})
+		.catch(error) {
+			console.log(error)
+			return error
+		}
+	},
+
+	getAccessToken: () => {
+		instance.get('/callback')
+		.then(response  => {
+			return response
+		})
+		.catch(error) {
+			console.log(error)
+			return error
+		}
+	},
+
+	getRefreshToken: () => {
+		instance.get('/refresh-token')
+		.then(response => {
+			return response
+		})
+		.catch(error) {
+			console.log(error)
+			return error
+		}
+	}
+}
