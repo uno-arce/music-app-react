5a642e9005163944f83e3ec20b2a784d0d342548
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-29 23:05:01 +0800

    Set form component to accept input and manage its states

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,20 +1,34 @@
 
-export default function Form({ input, call, store }) {
+export default function Form({ inputs, call }) {
 
-	const {
-		username,
-		email,
-		password,
-		isAuthenticated,
+	const handleSubmit = async(event) => {
+		event.preventDefault();
 
-		setUsername,
-		setEmail,
-		setPassword,
-		setIsAuthenticated
-	} = store
-
-	const formType = () => {
+		try {
+			let response
+			response = await call();
+		} catch(error) {
+			console.error(error)
+		}
 		
 	}
+
+	const formInputs = inputs.map(field => {
+		return (
+			<div key={field.name}>
+				<label htmlFor={field.name}>{field.name}</label>
+				<input
+				name={field.name}
+				onChange={event => field.updateState(event.target.value)}
+				/>
+			</div>
+		)
+	})
+
+	return (
+		<form onSubmit={handleSubmit}>
+			{formInputs}
+		</form>
+	)
 
 }
