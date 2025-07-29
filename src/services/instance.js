8b95af4944f66c5fd3e4b7f212cea2e950ea6364
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-29 23:04:23 +0800

    Change name of react base api url to vite

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: import.meta.env.REACT_API_BASE_URL,
+	baseURL: import.meta.env.VITE_API_BASE_URL,
 	timeout: 10000,
 	headers: {
 		'Content-Type': 'application/json'
 	}
 })
+
 
 export default instance
