b2717ae3526bf6eba93f4d170b6ce2957ad3d245
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-29 23:05:52 +0800

    Set login page props to be handled by form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,40 @@
 import Form from '../components/form'
-import userAuthStore from '../stores/userAuthStore'
+import useUserAuthStore from '../stores/userAuthStore'
 import userAuth from '../services/userAuth'
 
 
 export default function Login() {
 
+	const userAuthStore = useUserAuthStore()
+
+	const loginInputs = [
+		{
+			name: 'email',
+			email: userAuthStore.email,
+			updateState: (value) => {
+				userAuthStore.setEmail(value)
+			} 
+		},
+		{
+			name: 'password',
+			password: userAuthStore.password,
+			updateState: (value) => {
+				userAuthStore.setPassword(value)
+			}
+		}
+	]
+
+	const handleLoginSubmit = async () => {
+        return userAuth.login(userAuthStore.email, userAuthStore.password);
+    };
+
 	return(
-		<Form 
-			input = {['email', 'password']}
-			call = {userAuth.login}
-			store = {userAuthStore}
-		/>
+		<div>
+			<h1>Login</h1>
+			<Form 
+				inputs = {loginInputs}
+				call = {handleLoginSubmit}
+			/>
+		</div>
 	)
 }
