74e707222578e21018643a39bda9a7ffa50cec19
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-30 22:42:38 +0800

    Remove isAuthenticated property

--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -4,12 +4,10 @@ const useUserAuthStore = create((set) => ({
 	username: null,
 	email: null,
 	password: null,
-	isAuthenticated: false,
 
 	setUsername: (username) => set({ username: username }),
 	setEmail: (email) => set({ email: email }),
 	setPassword: (password) => set({ password: password }),
-	setIsAuthenticated: (status) => set({ isAuthenticated: status })
 }))
 
 export default useUserAuthStore
