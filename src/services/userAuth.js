0220acfd3219de575775a29d70346ab95335db70
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-07-31 22:03:06 +0800

    Added verify request method and modified endpoints

--- src/services/userAuth.js
+++ src/services/userAuth.js
@@ -1,24 +1,12 @@
 import instance from './instance'
-import Cookies from 'js-cookie'
-import { useNavigate } from 'react-router'
-import useUserAuthStore from '../stores/userAuthStore'
-
-let navigate = useNavigate()
-const userAuthStore = useUserAuthStore()
 
 const userAuth = {
 	login: (email, password) => {
-		instance.post('/login', {
+		return instance.post('users/login', {
 			email: email,
 			password: password
 		}).then(response => {
-			if(!response.status) {
-				return response.body.message
-			} else {
-				userAuthStore.setEmail(null)
-				userAuthStore.setPassword(null)
-				useNavigate('/')
-			}
+			return response
 		}).catch(error => {
 			console.log('Login call error: ' + error)
 			return error
@@ -26,23 +14,33 @@ const userAuth = {
 	},
 
 	register: (username, email, password) => {
-		instance.post('/register', {
+		return instance.post('users/register', {
 			username: username,
 			email: email,
 			password: password
 		}).then(response => {
-			return response.status
+			return response
 		}).catch(error => {
 			console.log('Register call error: ' + error)
 			return error
 		})
 	},
 
+	verify: () => {
+		return instance.get('/verify')
+		.then(response => {
+			return response
+		})
+		.catch(error => {
+			console.error(error)
+		})
+	},
+
 	addSongRatings: (ratedSongs) => {
-		instance.post('/rate-songs', {
+		return instance.post('users/rate-songs', {
 			ratedSongs: ratedSongs
 		}).then(response => {
-			return response.status
+			return response
 		}).catch(error => {
 			console.log(error)
 			return error
