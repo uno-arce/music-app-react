8d697b69b7239e65afd498f7559718df1006edbe
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-04 23:51:55 +0800

    Added form hook in progress

---
+++ src/hooks/formHooks.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { useUserAuthStore } from '../stores/userAuthStore'
+
+
+const useForm = () => {
+
+	const validate = (inputs) => {
+		return
+	}
+
+	return {
+		validate
+	}
+}
