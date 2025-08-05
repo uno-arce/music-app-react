bfc9613977975d36480c2829d68f75cc9306ae72
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-05 19:28:10 +0800

    Transfered logic from form to form hook

--- src/hooks/formHooks.js
+++ src/hooks/formHooks.js
@@ -1,14 +1,26 @@
 import { useEffect } from 'react'
-import { useUserAuthStore } from '../stores/userAuthStore'
+import useUserAuthStore from '../stores/userAuthStore'
 
 
 const useForm = () => {
-
+	
 	const validate = (inputs) => {
+		// inputs.map(field => {
+		// 	return field
+		// })
 		return
 	}
 
+	const handleFormSubmit = async (event, call) => {
+		event.preventDefault()
+
+		await call()
+	}
+
 	return {
-		validate
+		validate,
+		handleFormSubmit
 	}
 }
+
+export default useForm
