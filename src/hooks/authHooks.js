66bbade5306846cd466c25edef550731efb6310d
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-05 19:25:28 +0800

    Transfered logic from login page to user auth hook

--- src/hooks/authHooks.js
+++ src/hooks/authHooks.js
@@ -1,30 +1,56 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
-import { useUserAuthStore } from '../stores/userAuthStore'
-import { userAuth } from '../services/userAuth.js'
+import useUserAuthStore from '../stores/userAuthStore'
+import userAuth from '../services/userAuth.js'
 
 const useAuth = () => {
 	const navigate = useNavigate()
 	const userAuthStore = useUserAuthStore()
 
 
+	const loginInputs = [
+		{
+			name: 'Email',
+			value: userAuthStore.email,
+			updateState: (value) => {
+				userAuthStore.setEmail(value)
+			} 
+		},
+		{
+			name: 'Password',
+			value: userAuthStore.password,
+			type: 'password',
+			updateState: (value) => {
+				userAuthStore.setPassword(value)
+			}
+		}
+	]
+
+	const isButtonDisabled = !userAuthStore.email || !userAuthStore.password
+
 	const login = () => {
-		userAuthStore.setIsFormDisable(true)
+		userAuthStore.setIsFormDisabled(true)
 
 		userAuth.login(userAuthStore.email, userAuthStore.password)
 		.then(response => {
 			if(response.status !== 200) {
-        		return response.body.error
+				userAuthStore.resetLoginState()
+        		return response
         	} 
 
-        	userAuthStore.setEmail(null)
-        	userAuthStore.setPassword(null)
-        	userAuthStore.setIsFormDisabled(false)
-        	navigate('/' { replace: true })
+        	userAuthStore.resetLoginState()
+        	navigate('/', { replace: true })
+		}).catch(error => {
+			console.log(error)
+			userAuthStore.resetLoginState()
 		})
 	}
 
 	return {
+		loginInputs,
+		isButtonDisabled,
 		login
 	}
 }
+
+export default useAuth
