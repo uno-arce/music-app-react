dd9086253f13b587ef6a04fa3829b629c82a114a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-05 19:28:38 +0800

    Use custom hooks for children props

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,56 +2,22 @@ import React from 'react';
 import Form from '../components/form'
 import Button from '../components/button'
 import useUserAuthStore from '../stores/userAuthStore'
-import userAuth from '../services/userAuth'
-import { useNavigate } from 'react-router'
+import useAuth from '../hooks/authHooks'
+import useForm from '../hooks/formHooks'
 
 
 export default function Login() {
+	const { login, loginInputs, isButtonDisabled } = useAuth()
+	const { validate, handleFormSubmit } = useForm()
 	const userAuthStore = useUserAuthStore()
-	let navigate = useNavigate()
-
-	const isButtonDisabled = !userAuthStore.email || !userAuthStore.password
-
-	const loginInputs = [
-		{
-			name: 'email',
-			email: userAuthStore.email,
-			updateState: (value) => {
-				userAuthStore.setEmail(value)
-			} 
-		},
-		{
-			name: 'password',
-			password: userAuthStore.password,
-			type: 'password',
-			updateState: (value) => {
-				userAuthStore.setPassword(value)
-			}
-		}
-	]
-
-	const handleLoginSubmit = async () => {
-		userAuthStore.setIsFormDisabled(true)
-
-        userAuth.login(userAuthStore.email, userAuthStore.password).
-        then(response => {
-        	if(response.status !== 200) {
-        		return response.body.error
-        	} 
-
-        	userAuthStore.setEmail(null)
-        	userAuthStore.setPassword(null)
-        	userAuthStore.setIsFormDisabled(false)
-        	navigate('/')
-        })
-    };
 
 	return(
 		<div>
 			<h1>Login</h1>
 			<Form 
 				inputs = {loginInputs}
-				call = {handleLoginSubmit}
+				call = {login}
+				submit = {handleFormSubmit}
 				isDisabled = {userAuthStore.isFormDisabled}
 			>
 			<Button
