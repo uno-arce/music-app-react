a14dcacfcd72af2dc5dfba5965852d36956bb103
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-06 18:41:00 +0800

    Modified protected route logic

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,19 @@
-import useAuth from '../hooks/authHooks'
+import react from 'react'
+import useUserAuthStore from '../stores/userAuthStore'
 import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = ({ children }) => {
+	const userAuthStore = useUserAuthStore()
+
+	if(userAuthStore.isLoading) {
+		return <div>Loading, please wait...</div>
+	}
+
+	if(!userAuthStore.isAuthenticated) {
+		return <Navigate to='login' replace />
+	}
 
 	return children
 }
+
+export default ProtectedRoute
