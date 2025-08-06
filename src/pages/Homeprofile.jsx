0ffa10d54fa7407447f40ab5570b75620650ec7e
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-06 18:43:10 +0800

    Added homeprofile page initial view

---
+++ src/pages/Homeprofile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import useTracksStore from '../stores/tracksStore'
+import Form from '../components/form'
+import Button from '../components/button'
+import 
+
+export default function Homeprofile() {
+
+	return(
+		<div>
+			<Button
+				name={"Connect to spotify"}
+				call={}
+			/>
+		</div
+	)
+}
