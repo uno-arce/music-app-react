d6867a7b7cba16a6ba0448d404dd24e39d31b45d
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-06 18:42:51 +0800

    Added state and actions

--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -5,12 +5,16 @@ const useUserAuthStore = create((set) => ({
 	email: '',
 	password: '',
 
+	isAuthenticated: false,
 	isFormDisabled: false,
+	isLoading: true,
 
 	setUsername: (username) => set({ username: username }),
 	setEmail: (email) => set({ email: email }),
 	setPassword: (password) => set({ password: password }),
 	setIsFormDisabled: (form) => set({ isFormDisabled: form }),
+	setIsAuthenticated: (authenticated) => set({ isAuthenticated: authenticated }),
+	setIsLoading: (loading) => set({ isLoading: loading }),
 
 	resetLoginState: () => {
 		set({
