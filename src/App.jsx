1cf4f6cc72c769d67d5b4363f310a01c45fda7bf
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-06 18:39:58 +0800

    Added protected and public only route components and wrapped pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 // Pages
 import Login from './pages/Login'
 import Homepage from './pages/Homepage'
+import Homeprofile from './pages/Homeprofile'
 import './App.css'
 
 // Routing
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { useEffect } from 'react'
-import userAuth from './services/userAuth'
+import PublicRoute from './components/PublicRoute'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-  useEffect(() => {
-    // return userAuth.verify()
-  }, [])
-
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
-        <Route path='/login' element={<Login/>}/>
+
+        <Route
+          path='/login' 
+          element={
+            <PublicRoute>
+              <Login/>
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path='/homeprofile'
+          element={
+            <ProtectedRoute>
+              <Homeprofile/>
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </ Router>
   )
