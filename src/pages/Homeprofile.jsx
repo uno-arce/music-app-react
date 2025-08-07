752dccae18320a6d473642d9756fd5271a709975
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-07 18:34:39 +0800

    Modified incorrect syntax

--- src/pages/Homeprofile.jsx
+++ src/pages/Homeprofile.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import useTracksStore from '../stores/tracksStore'
 import Form from '../components/form'
 import Button from '../components/button'
-import 
 
 export default function Homeprofile() {
 
@@ -12,6 +11,6 @@ export default function Homeprofile() {
 				name={"Connect to spotify"}
 				call={}
 			/>
-		</div
+		</div>
 	)
 }
