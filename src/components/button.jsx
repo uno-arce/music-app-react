0ce1468d7f45c93c413b48dd95d0637f5869699b
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-07 18:33:54 +0800

    Modified incorrect attribute name

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Button({name, id, call, isDisabled}) {
 	return(
 		<div>
-			<button type='submit' disabled={isDisabled} onclick={call}>{name}</button>
+			<button type='submit' disabled={isDisabled} onClick={call}>{name}</button>
 		</div>
 	)
 }
