e50b28755bc0bdbed3ed36b33a4627a38075155e
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-08 23:39:26 +0800

    Added logout button and functionality

--- src/pages/Homeprofile.jsx
+++ src/pages/Homeprofile.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import useTracksStore from '../stores/tracksStore'
+import useAuth from '../hooks/authHooks'
 import Form from '../components/form'
 import Button from '../components/button'
 
 export default function Homeprofile() {
+	const { logout } = useAuth()
+
+	const handleSpotifyConnect = () => {
+	    // This is the correct way to trigger the server-side redirect
+	    // Ensure your server's base URL is correct
+	    window.location.href = 'http://localhost:4000/auth/spotify/';
+	}
 
 	return(
 		<div>
 			<Button
 				name={"Connect to spotify"}
-				call={}
+				call={handleSpotifyConnect}
+			/>
+			<Button
+				name={"Logout"}
+				call={logout}
 			/>
 		</div>
 	)
