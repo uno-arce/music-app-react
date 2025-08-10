e581108dc346d351edf065bf8d830ebadfb77abc
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-10 20:51:37 +0800

    Added authenticate and save spotify tokens logic

--- src/hooks/spotifyAuthHooks.js
+++ src/hooks/spotifyAuthHooks.js
@@ -1,15 +1,31 @@
 import { useEffect } from 'react'
-import spotifyAuth from '../services/spotifyAuth'
 import useSpotifyAuthStore from '../stores/spotifyAuthStore'
+import spotifyApi from '../services/spotifyApi'
 
 const useSpotifyAuth = () => {
 	const spotifyAuthStore = useSpotifyAuthStore()
 
 	const authenticate = () => {
-		return
+		window.location.href = 'http://127.0.0.1:4000/auth/spotify/';
+	}
+
+	const saveSpotifyTokens = () => {
+		spotifyApi.saveSpotifyTokens(accessToken, refreshToken)
+		.then(response => {
+			if(response.status !== 200) {
+				return
+			}
+
+			spotifyAuthStore.setIsAuthenticated(true)
+		}).catch(error => {
+			console.log(error)
+			return error
+		})
 	}
 
 	return{
 		authenticate
 	}
 }
+
+export default useSpotifyAuth
