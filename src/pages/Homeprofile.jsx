2eaf02d0e606f045649a059c2061608dc871136a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-10 20:52:49 +0800

    Called authenticate from useSpotify auth

--- src/pages/Homeprofile.jsx
+++ src/pages/Homeprofile.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import useTracksStore from '../stores/tracksStore'
 import useAuth from '../hooks/authHooks'
+import useSpotifyAuth from '../hooks/spotifyAuthHooks'
 import Form from '../components/form'
 import Button from '../components/button'
 
 export default function Homeprofile() {
 	const { logout } = useAuth()
-
-	const handleSpotifyConnect = () => {
-	    // This is the correct way to trigger the server-side redirect
-	    // Ensure your server's base URL is correct
-	    window.location.href = 'http://localhost:4000/auth/spotify/';
-	}
+	const { authenticate } = useSpotifyAuth()
 
 	return(
 		<div>
 			<Button
 				name={"Connect to spotify"}
-				call={handleSpotifyConnect}
+				call={authenticate}
 			/>
 			<Button
 				name={"Logout"}
