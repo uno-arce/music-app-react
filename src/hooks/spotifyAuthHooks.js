a63572203cf058221aa78336c86bda0293b66504
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-14 21:06:51 +0800

    Added side effect for verifying spotify authorization

--- src/hooks/spotifyAuthHooks.js
+++ src/hooks/spotifyAuthHooks.js
@@ -5,6 +5,24 @@ import spotifyApi from '../services/spotifyApi'
 const useSpotifyAuth = () => {
 	const spotifyAuthStore = useSpotifyAuthStore()
 
+	useEffect(() => {
+		if(!spotifyAuthStore.isAuthorized) {
+			spotifyApi.verifyAuthorization()
+			.then(response => {
+				if(response.status !== 200) {
+					return
+				}
+
+				spotifyAuthStore.setIsAuthorized(true)
+			}).catch(error => {
+				console.log(error)
+				return error
+			})
+		}
+	}, [spotifyAuthStore.isAuthorized, spotifyAuthStore])
+
+	console.log(spotifyAuthStore.isAuthorized)
+
 	const authenticate = () => {
 		window.location.href = 'http://127.0.0.1:4000/auth/spotify/';
 	}
@@ -25,7 +43,8 @@ const useSpotifyAuth = () => {
 
 	return{
 		authenticate,
-		saveSpotifyTokens
+		saveSpotifyTokens,
+		isAuthorized: spotifyAuthStore.isAuthorized
 	}
 }
 
