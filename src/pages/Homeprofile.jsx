65c021b5a0b333f0d32f2cbfd188f12edbbd756a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-17 19:22:21 +0800

    Modified to use spotify api hook

--- src/pages/Homeprofile.jsx
+++ src/pages/Homeprofile.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import useTracksStore from '../stores/tracksStore'
 import useAuth from '../hooks/authHooks'
 import useSpotifyAuth from '../hooks/spotifyAuthHooks'
+import useSpotifyApi from '../hooks/spotifyApiHooks'
 import Form from '../components/form'
 import Button from '../components/button'
 
 export default function Homeprofile() {
 	const { logout } = useAuth()
 	const { authenticate } = useSpotifyAuth()
+	const { isLoading, recentlyPlayedTracks } = useSpotifyApi()
 
 	return(
 		<div>
