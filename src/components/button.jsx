1a1dcfd0c7098e1af1e611d5a3a5542cfe585ace
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-19 19:02:07 +0800

    Added button style

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { buttonStyle } from '../styles/style'
 
 export default function Button({name, id, call, isDisabled}) {
+	const buttonClasses = buttonStyle(isDisabled)
+
 	return(
 		<div>
-			<button type='submit' disabled={isDisabled} onClick={call}>{name}</button>
+			<button className={buttonClasses} type='submit' disabled={isDisabled} onClick={call}>{name}</button>
 		</div>
 	)
 }
