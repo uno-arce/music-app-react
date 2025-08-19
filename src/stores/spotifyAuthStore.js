42e928f6fa009c013ddafb9a34b244473e96035c
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-19 19:04:21 +0800

    Added is auth loading property

--- src/stores/spotifyAuthStore.js
+++ src/stores/spotifyAuthStore.js
@@ -3,10 +3,12 @@ import { create } from 'zustand'
 const useSpotifyAuthStore = create((set) => ({
 	isAuthorized: false,
 	isTokenExpired: true,
+	isAuthLoading: true,
 
 
 	setIsAuthorized: (authorization) => set({ isAuthorized: authorization }),
 	setIsTokenExpired: (expiration) => set({ isTokenExpired: expiration }),
+	setIsAuthLoading: (loading) => set({ isAuthLoading: loading })
 }))
 
 export default useSpotifyAuthStore
