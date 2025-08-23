3882008c737269a66acd41cde3d5f7f6607ef3c4
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-23 21:16:39 +0800

    Added properties for data collection

--- src/stores/tracksStore.js
+++ src/stores/tracksStore.js
@@ -2,8 +2,10 @@ import { create } from 'zustand'
 
 const useTracksStore = create((set) => ({
 	ratedSongs: [],
+	rating: 0,
 
-	setRatedSongs: (ratedSong) => ({ ratedSongs: ratedSongs.push({ratedSong}) })
+	setRatedSongs: (ratedSong) => ({ ratedSongs: ratedSongs.push({ratedSong}) }),
+	setRating: (rating) =>  set({ rating: rating }),
 }))
 
 export default useTracksStore
