ed0a9df214fc436f1f8ce7a2421474b459fa35bf
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-25 18:47:41 +0800

    Modified collection structure and style initial

--- src/components/collection.jsx
+++ src/components/collection.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { containerStyle, imageStyle, textStyle } from '../styles/style'
 
 export default function Collection({ items, isLoading }) {
+	const { flex } = containerStyle()
+	const imageClasses = imageStyle()
+	const textClasses = textStyle()
 
 	const dataCollection = items.map(item => {
 		return(
-			<div key={item.track.name}>
-				<p>{item.track.name}</p>
+			<div className={flex} key={item.track.id}>
+				<img className={imageClasses} src={item.track.album.images[0].url}/>
+				<div>
+					<p className={textClasses}>{item.track.name}</p>
+					<p className={textClasses}>{item.track.artists[0].name}</p>
+				</div>
 			</div>
 		)
 	})
 	return(
-		{dataCollection}
+		<>
+			{dataCollection}
+		</>
 	)
 }
