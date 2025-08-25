3978ee010af4986ebec1742d8a005234962f362d
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-08-25 18:48:57 +0800

    Added other styles

--- src/styles/style.js
+++ src/styles/style.js
@@ -1,6 +1,29 @@
+
 // Buttons
 export const buttonStyle = (isDisabled) => {
 	const enabled = 'cursor-pointer'
 	const disabled = 'cursor-default'
 	return isDisabled ? `${disabled}` : `${enabled}`
+}
+
+// Container
+export const containerStyle = () => {
+	const flex = 'flex'
+	const flexColumn = 'flex flex-col'
+	return {
+		flex,
+		flexColumn
+	}
+}
+
+// Images
+export const imageStyle = () => {
+	const imgDefault = 'w-64 h-64'
+	return imgDefault
+}
+
+// Texts 
+export const textStyle = () => {
+	const textDefault = 'neutral-700'
+	return textDefault
 }
