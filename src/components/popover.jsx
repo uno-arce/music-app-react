bb71f970b1b7f6ea5c2ada510579ba41103e434b
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-03 22:29:25 +0800

    Added popover display styles

--- src/components/popover.jsx
+++ src/components/popover.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { popoverStyle } from '../styles/style'
 
 export default function Popover({ children }) {
+	const { popoverBackground, popoverDefault } = popoverStyle()
 	return(
-		<div>
-			{children}
+		<div className={popoverBackground}>
+			<div className={popoverDefault}>
+				{children}
+			</div>
 		</div>
 	)
 }
