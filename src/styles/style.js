a54f93e6607f2f05f9d657d418f0fd2f80d48e2c
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-04 23:27:34 +0800

    Added rating symbol style

--- src/styles/style.js
+++ src/styles/style.js
@@ -24,7 +24,8 @@ export const imageStyle = () => {
 
 // Popover
 export const popoverStyle = () => {
-	const popoverBackground = 'fixed w-screen h-screen inset-0 backdrop-blur-xs flex justify-center items-center'
+	const popoverBackground = 'fixed w-screen h-screen inset-0 backdrop-blur-xs flex flex-col justify-center items-center'
+	const popoverButton = 'flex justify-end'
 	const popoverDefault = 'relative min-h-fit min-w-fit p-4 bg-neutral-100'
 	return {
 		popoverBackground,
@@ -34,8 +35,12 @@ export const popoverStyle = () => {
 
 // Rating
 export const ratingStyle = () => {
-	const ratingSymbol= ''
-	const ratingDefault = 'flex'
+	const ratingSymbol= 'fill-blue-300 hover:fill-blue-300 transition-all'
+	const ratingSymbolFourth= 'peer/fourth fill-blue-100 hover:fill-blue-200 peer-hover/fifth:fill-blue-200 transition-all'
+	const ratingSymbolThird= 'peer/third fill-blue-100 hover:fill-blue-200 peer-hover/fifth:fill-blue-200 peer-hover/fourth:fill-blue-200 transition-all'
+	const ratingSymbolSecond= 'peer/second fill-blue-100 hover:fill-blue-200 peer-hover/fifth:fill-blue-200 peer-hover/fourth:fill-blue-200 peer-hover/third:fill-blue-200 transition-all'
+	const ratingSymbolFirst= 'peer/first fill-blue-100 hover:fill-blue-200 peer-hover/fifth:fill-blue-200 peer-hover/fourth:fill-blue-200 peer-hover/third:fill-blue-200 peer-hover/second:fill-blue-200 transition-all'
+	const ratingDefault = 'flex flex-row-reverse mx-auto'
 	return {
 		ratingSymbol,
 		ratingDefault
