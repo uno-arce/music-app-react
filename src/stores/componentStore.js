f09907848b3962d2100bb223aea6d4ee69ed5518
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-04 23:27:10 +0800

    Added rating properties

--- src/stores/componentStore.js
+++ src/stores/componentStore.js
@@ -5,7 +5,13 @@ const useComponentStore = create((set) => ({
 	popoverItem: null,
 	isPopoverOpen: false,
 	setPopoverItem: (popoverItem) => set({ popoverItem: popoverItem }),
-	setIsPopoverOpen: (isOpen) => set({ isPopoverOpen: isOpen })
+	setIsPopoverOpen: (isOpen) => set({ isPopoverOpen: isOpen }),
+
+	//Rating
+	hoveredIndex: 0,
+	clickedIndex: 0,
+	setHoveredIndex: (hoveredIndex) => set({ hoveredIndex: hoveredIndex }),
+	setClickedIndex: (clickedIndex) => set({ clickedIndex: clickedIndex })
 }))
 
 export default useComponentStore
