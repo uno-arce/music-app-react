767e093f74faa59d8609431cc7383396aaba883c
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-04 23:25:57 +0800

    Added handler functions for rating component

--- src/hooks/ratingHooks.js
+++ src/hooks/ratingHooks.js
@@ -1,12 +1,32 @@
+import useComponentStore from '../stores/componentStore'
 
 const useRating = () => {
+	const componentStore = useComponentStore()
 
-	const handleRatingSubmit = async (call) {
+	const handleHoverRating = (index) => {
+		componentStore.setHoveredIndex(index)
+	}
+
+	const handleLeaveRating = () => {
+		componentStore.setHoveredIndex(-1)
+	}
+
+	const handleRatingPath = (index) => {
+		const filledPath = "m233-120 65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z";
+		const emptyPath = "m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z";
+
+		return index <= componentStore.hoveredIndex > 0 ? filledPath : emptyPath
+	}
+
+	const handleRatingSubmit = async (call) => {
 		await call()
 	}
 
 	return {
-
+		handleHoverRating,
+		handleLeaveRating,
+		handleRatingPath,
+		handleRatingSubmit
 	}
 }
 
