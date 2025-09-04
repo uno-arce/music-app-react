d51c9552b3bf83292a52f6b09c90c1a438468b01
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-04 23:24:40 +0800

    Added close props and button

--- src/components/popover.jsx
+++ src/components/popover.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { popoverStyle } from '../styles/style'
 
-export default function Popover({ children }) {
-	const { popoverBackground, popoverDefault } = popoverStyle()
+export default function Popover({ close, children }) {
+	const { popoverBackground, popoverDefault, popoverButton } = popoverStyle()
 	return(
 		<div className={popoverBackground}>
+			<div className={popoverButton}>close</div>
 			<div className={popoverDefault}>
 				{children}
 			</div>
