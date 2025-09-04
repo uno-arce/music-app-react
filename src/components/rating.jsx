46eb07d13f96a82f974cd57a85788ddee1b007df
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-04 23:25:04 +0800

    Refactor rating props and display logic

--- src/components/rating.jsx
+++ src/components/rating.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import { containerStyle, imageStyle, ratingStyle } from '../styles/style'
 
-export default function Rating({ item, call }) {
+export default function Rating({ item, handleOnHover, handleOnLeave, handleFillRating, call }) {
 	const { flex, flexColumn } = containerStyle()
 	const imageClasses = imageStyle()
+	const { ratingDefault, ratingSymbol} = ratingStyle()
 
-	const symbol = (
-		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#666666"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"/></svg>
-	)
+	const symbols = [5, 4, 3, 2, 1]
 
-	const filledSymbol = (
-		<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#AAC1F0"><path d="m233-120 65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/></svg>
-	)
-
-	const rating = (
-		<div>
-			<div>{symbol}</div>
-		</div>
-	)
+	const rating = symbols.map(symbol => {
+		return(
+			<svg 
+				key={symbol}
+				className={ratingSymbol} 
+				xmlns="http://www.w3.org/2000/svg" 
+				height="48px"
+				viewBox="0 -960 960 960"
+				width="48px" 
+				fill="currentColor"
+				onMouseEnter={() => handleOnHover(symbol)}
+				onMouseLeave={() => handleOnLeave()}
+			>
+				<path d={handleFillRating(symbol)}/>
+			</svg>
+		)
+	})
 
 	return(
 		<div className={flex}>
 			<img className={imageClasses} src={item.track.album.images[0].url}/>
 			<div className={flexColumn}>
 				<p>Your rating to {item.track.name} by {item.track.artists[0].name}</p>
-				{rating}
+				<div className={ratingDefault}>
+					{rating}
+				</div>
 			</div>
 		</div>
 	)
