c7a81f9eff68f34ad78ad497f6244892d94bc530
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-05 22:23:03 +0800

    Refactor popover close button style and function

--- src/components/popover.jsx
+++ src/components/popover.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
-import { popoverStyle } from '../styles/style'
+import { popoverStyle, containerStyle } from '../styles/style'
 
 export default function Popover({ close, children }) {
 	const { popoverBackground, popoverDefault, popoverButton } = popoverStyle()
+	const { flexColumn } = containerStyle()
 	return(
 		<div className={popoverBackground}>
-			<div className={popoverButton}>close</div>
-			<div className={popoverDefault}>
-				{children}
+			<div className={flexColumn}>
+				<svg
+					className={popoverButton}
+					xmlns="http://www.w3.org/2000/svg" 
+					height="32px" 
+					viewBox="0 -960 960 960" 
+					width="32px" 
+					fill="currentColor"
+					onClick={close}
+				>
+					<path d="M400-240 160-480l240-240 56 58-142 142h486v80H314l142 142-56 58Z"/>
+				</svg>
+				<div className={popoverDefault}>
+					{children}
+				</div>
 			</div>
 		</div>
 	)
