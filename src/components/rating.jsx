5224feca7c3964e68767c56d58edd264595a0a7c
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-05 22:23:21 +0800

    Refactor rating display and symbol style

--- src/components/rating.jsx
+++ src/components/rating.jsx
@@ -4,24 +4,56 @@ import { containerStyle, imageStyle, ratingStyle } from '../styles/style'
 export default function Rating({ item, handleOnHover, handleOnLeave, handleFillRating, call }) {
 	const { flex, flexColumn } = containerStyle()
 	const imageClasses = imageStyle()
-	const { ratingDefault, ratingSymbol} = ratingStyle()
+	const { 
+		ratingDefault, 
+		ratingGroup, 
+		ratingSymbolFifth,
+		ratingSymbolFourth,
+		ratingSymbolThird,
+		ratingSymbolSecond,
+		ratingSymbolFirst,
+		ratingTitle, 
+		ratingSubtitle
+	} = ratingStyle()
 
-	const symbols = [5, 4, 3, 2, 1]
+	const symbols = [
+		{
+			index: 5,
+			style: ratingSymbolFifth
+		},
+		{
+			index: 4,
+			style: ratingSymbolFourth
+		},
+		{
+			index: 3,
+			style: ratingSymbolThird
+		},
+		{
+			index: 2,
+			style: ratingSymbolSecond
+		},
+		{
+			index: 1,
+			style: ratingSymbolFirst
+		}
+	]
 
 	const rating = symbols.map(symbol => {
 		return(
 			<svg 
-				key={symbol}
-				className={ratingSymbol} 
+				key={symbol.index}
+				className={symbol.style} 
 				xmlns="http://www.w3.org/2000/svg" 
 				height="48px"
 				viewBox="0 -960 960 960"
 				width="48px" 
 				fill="currentColor"
-				onMouseEnter={() => handleOnHover(symbol)}
+				onMouseEnter={() => handleOnHover(symbol.index)}
 				onMouseLeave={() => handleOnLeave()}
+				onClick={call}
 			>
-				<path d={handleFillRating(symbol)}/>
+				<path d={handleFillRating(symbol.index)}/>
 			</svg>
 		)
 	})
@@ -29,11 +61,12 @@ export default function Rating({ item, handleOnHover, handleOnLeave, handleFillR
 	return(
 		<div className={flex}>
 			<img className={imageClasses} src={item.track.album.images[0].url}/>
-			<div className={flexColumn}>
-				<p>Your rating to {item.track.name} by {item.track.artists[0].name}</p>
+			<div className={ratingGroup}>
+				<p className={ratingTitle}>Your rating to {item.track.name} by {item.track.artists[0].name}</p>
 				<div className={ratingDefault}>
 					{rating}
 				</div>
+				<p className={ratingSubtitle}>Rated tracks will be saved in your favorites</p>
 			</div>
 		</div>
 	)
