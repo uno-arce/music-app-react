a964add8be582601c4495200737a2613e96ae66a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-08 01:19:27 +0800

    Refactor rating to be independent component

--- src/components/collection.jsx
+++ src/components/collection.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { containerStyle, imageStyle, textStyle } from '../styles/style'
 
-export default function Collection({ items, isSelectable, openPopover, isOpen, children }) {
+export default function Collection({ items, isSelectable, openCollection, isOpen, children }) {
 	const { flex } = containerStyle()
 	const imageClasses = imageStyle()
 	const textClasses = textStyle()
 
 	const dataCollection = items.map(item => {
 		return(
-			<div onClick={() => openPopover(item, isSelectable)} className={flex} key={item.track.id}>
+			<div onClick={() => openCollection(item, isSelectable)} className={flex} key={item.track.id}>
 				<img className={imageClasses} src={item.track.album.images[0].url}/>
 				<div>
 					<p className={textClasses}>{item.track.name}</p>
