8edca2d43e299af87668d8b44891d8df5d2a84a9
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-17 22:00:09 +0800

    Added is length correct state for form validation

--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -9,12 +9,15 @@ const useUserAuthStore = create((set) => ({
 	isFormDisabled: false,
 	isLoading: true,
 
+	isUsernameLengthCorrect: false,
+
 	setUsername: (username) => set({ username: username }),
 	setEmail: (email) => set({ email: email }),
 	setPassword: (password) => set({ password: password }),
 	setIsFormDisabled: (form) => set({ isFormDisabled: form }),
 	setIsAuthenticated: (authenticated) => set({ isAuthenticated: authenticated }),
 	setIsLoading: (loading) => set({ isLoading: loading }),
+	setIsUsernameLengthCorrect: (isUsernameLengthCorrect)  => set({ isUsernameLengthCorrect: isUsernameLengthCorrect }),
 
 	resetUserAuthState: () => {
 		set({
