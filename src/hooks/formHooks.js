d19bbabad228fca4d847244ccf61d07178beb8bf
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-18 23:28:55 +0800

    Added more form validation logic

--- src/hooks/formHooks.js
+++ src/hooks/formHooks.js
@@ -16,9 +16,20 @@ const useForm = () => {
 		return
 	}
 	
-	const validateTextCharacters = (input) => {
-		
-		return
+	const validateMixedCharacters = (input) => {
+		const hasLetter =  /[a-zA-Z]/.test(input)
+		const hasNumber = /[0-9]/.test(input)
+		const hasSymbol = /[^a-zA-Z0-9]/.test(input)
+		const isNotOnlyNumbers = !/^[0-9]+$/.test(input)
+		const isNotOnlySymbols = !/^[^a-zA-Z0-9]+$/.test(input)
+
+		return (hasLetter || hasNumber || hasSymbol) && isNotOnlyNumbers &&  isNotOnlySymbols
+	}
+
+	const validateEmailFormat = (input) => {
+		const isEmailLikeFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input)
+
+		return isEmailLikeFormat
 	}
 
 	const validateUniqueness = async (input, call) => {
@@ -34,6 +45,8 @@ const useForm = () => {
 
 	return {
 		validateTextLength,
+		validateMixedCharacters,
+		validateEmailFormat,
 		handleFormSubmit
 	}
 }
