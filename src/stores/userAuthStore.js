d509beb9151729fd94337dc90b7f5e5f85ddac7e
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-09-24 19:38:18 +0800

    added user form validation states

--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -12,6 +12,11 @@ const useUserAuthStore = create((set) => ({
 	isUsernameLengthCorrect: false,
 	isUsernameCharactersCorrect: false,
 	isEmailFormatCorrect: false,
+	isEmailAvailable: false,
+	isPasswordCharactersCorrect: false,
+	isPasswordTextCaseCorrect: false,
+	isPasswordLengthCorrect: false,
+	isPasswordVisible: true,
 
 	setUsername: (username) => set({ username: username }),
 	setEmail: (email) => set({ email: email }),
@@ -19,9 +24,16 @@ const useUserAuthStore = create((set) => ({
 	setIsFormDisabled: (form) => set({ isFormDisabled: form }),
 	setIsAuthenticated: (authenticated) => set({ isAuthenticated: authenticated }),
 	setIsLoading: (loading) => set({ isLoading: loading }),
+
 	setIsUsernameLengthCorrect: (isLengthCorrect) => set({ isUsernameLengthCorrect: isLengthCorrect }),
 	setIsUsernameCharactersCorrect: (isCharactersCorrect) => set({ isUsernameCharactersCorrect: isCharactersCorrect }),
 	setIsEmailFormatCorrect: (isEmailFormatCorrect) => set({ isEmailFormatCorrect: isEmailFormatCorrect }),
+	setIsEmailAvailable: (isEmailAvailable) => set({ isEmailAvailable: isEmailAvailable }),
+	setIsPasswordCharactersCorrect: (isCharactersCorrect) => set({ isPasswordCharactersCorrect: isCharactersCorrect }),
+	setIsPasswordTextCaseCorrect: (isTextCaseCorrect) => set({ isPasswordTextCaseCorrect: isTextCaseCorrect }),
+	setIsPasswordLengthCorrect: (isLengthCorrect) => set({ isPasswordLengthCorrect: isLengthCorrect }),
+	setIsPasswordVisible: (isVisible) => set({ isPasswordVisible: isVisible }),
+
 
 	resetUserAuthState: () => {
 		set({
