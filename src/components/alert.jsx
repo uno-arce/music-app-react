3a4961caf27134b662a21aed5023e235e5fd1ac5
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-10-06 18:02:17 +0800

    Refactor alert to be independent component

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import useAlert from '../hooks/alertHooks'
+import { alertStyle } from '../styles/style'
 
-export default function Alert({ renderAlert }) {
-	const { isAlertOpen } = useAlert()
+export default function Alert() {
+	const { alertStatus, alertMessage, handleAlertPath, isAlertOpen } = useAlert()
+
+	const { alertPosition, alertGroup, alertIcon, alertDescription } = alertStyle(alertStatus)
+
+	const alert = () => (
+		<div className={alertPosition}>
+			<div className={alertGroup}>
+				<svg 
+					className={alertIcon}
+					xmlns="http://www.w3.org/2000/svg" 
+					height="24px" 
+					viewBox="0 -960 960 960" 
+					width="24px" 
+					fill="currentColor">
+					<path d={handleAlertPath()}/>
+				</svg>
+				<p className={alertDescription}>{alertMessage}</p>
+			</div>
+		</div>
+	)
+
 
 	return(
 		<>
-			{isAlertOpen && renderAlert()}
+			{isAlertOpen && alert()}
 		</>
 	)
 }
