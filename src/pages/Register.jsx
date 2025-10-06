f972003f0a0fe8d1e2366bcef49d2dad2a1bd2ea
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-10-06 18:05:32 +0800

    Refactor registration form to be step form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,11 +9,10 @@ import { containerStyle } from '../styles/style'
 
 export default function Register() {
 	const { 
-		registerInputs, register, toggleShowPassword, isFormDisabled, isRegisterButtonDisabled,
-		isUsernameLengthCorrect, isUsernameCharactersCorrect, isEmailFormatCorrect,
-		isEmailAvailable, isPasswordCharactersCorrect, isPasswordTextCaseCorrect, 
-		isPasswordLengthCorrect, isPasswordVisible
+		registerInputs, register, toggleShowPassword, isFormDisabled, isRegisterButtonDisabled, isUsernameCorrect, isEmailCorrect, isPasswordCorrect, isUsernameLengthCorrect, isUsernameCharactersCorrect, isEmailFormatCorrect, isEmailAvailable, isPasswordCharactersCorrect, isPasswordTextCaseCorrect, isPasswordLengthCorrect, isPasswordVisible
 	} = useAuth()
+
+	const { handleNextStepForm } = useForm()
 
 	const { flex } = containerStyle()
 
@@ -23,7 +22,7 @@ export default function Register() {
 		const showPassword = <svg onClick={toggleShowPassword} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#CCDAF5"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"/></svg>
 		const hidePassword = <svg onClick={toggleShowPassword} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#CCDAF5"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"/></svg>
 
-		if(field == 'Username')  {
+		if(field == 'Username' )  {
 			return (
 				<div>
 					<div className={flex}>
@@ -33,6 +32,13 @@ export default function Register() {
 					<div className={flex}>
 						{ isUsernameLengthCorrect ? validSymbol : invalidSymbol }
 						<p>At least 6 characters and maximum of 20 characters</p>
+					</div>
+					<div className={flex}>
+						<Button
+							name={'Next'}
+							call={() => handleNextStepForm('Email')}
+							isDisabled={isUsernameCorrect}
+						/>
 					</div>
 				</div>
 			)
@@ -48,6 +54,17 @@ export default function Register() {
 					<div className={flex}>
 						{ isEmailAvailable ? validSymbol : invalidSymbol }
 						<p>Email is unique and available</p>
+					</div>
+					<div className={flex}>
+						<Button
+							name={'Go Back'}
+							call={() => handleNextStepForm('Username')}
+						/>
+						<Button
+							name={'Next'}
+							call={() => handleNextStepForm('Password')}
+							isDisabled={isEmailCorrect}
+						/>
 					</div>
 				</div>
 			)
@@ -72,6 +89,17 @@ export default function Register() {
 						{ isPasswordLengthCorrect ? validSymbol : invalidSymbol}
 						<p>Atleast 6 characters and maximum of 20 characters</p>
 					</div>
+					<div className={flex}>
+						<Button
+							name={'Go Back'}
+							call={() => handleNextStepForm('Email')}
+						/>
+						<Button
+							name={'Proceed'}
+							call={() => handleNextStepForm('Confirm Details')}
+							isDisabled={isPasswordCorrect}
+						/>
+					</div>
 				</div>
 			)
 		}
@@ -84,12 +112,9 @@ export default function Register() {
 				inputs={registerInputs}
 				call={register}
 				isDisabled={isFormDisabled}
+				isStepForm={true}
 				formValidator={renderRegisterFormValidatorView}
 			>
-			<Button
-				name={'Register'}
-				isDisabled={isRegisterButtonDisabled}
-			/>
 			</Form>
 		</div>
 	)
