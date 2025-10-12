c0a0947a468841652b0d655a9a397ce08d41bfde
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-10-12 21:17:20 +0800

    Added track progress, slider, and duration

--- src/components/track.jsx
+++ src/components/track.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import useTrack from '../hooks/trackHooks'
 
+import { containerStyle } from '../styles/style'
+
 export default function Track() {
-	const { trackRef, trackPreviewDetails, isTrackPlaying, isTrackMuted, togglePlayPause, toggleVolumeOnOff, handleReplay, handleVolumeChange, handleTrackEnd } = useTrack()
+	const { trackRef, trackPreviewDetails, trackDuration, trackCurrentTime, isTrackPlaying, isTrackMuted, handleTrackDuration, handleTrackCurrentTime, handleTrackTime, handleTimeSeek, togglePlayPause, toggleVolumeOnOff, handleReplay, handleVolumeChange, handleTrackEnd } = useTrack()
+
+	const { flex } = containerStyle()
 
 	const playButton = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
 	const pauseButton = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
@@ -11,12 +15,15 @@ export default function Track() {
 	const volumeOffButton = <svg onClick={toggleVolumeOnOff} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z"/></svg>
 
 	return(
-		<div>
+		<div className={flex}>
 			<audio
 				ref={trackRef}
-				src={trackPreviewDetails.previewUrl}
+				src={trackPreviewDetails?.previewUrl}
 				type='audio/mpeg'
 				onEnded={handleTrackEnd}
+				onTimeUpdate={handleTrackCurrentTime}
+				onLoadedMetadata={handleTrackDuration}
+				preload='metadata'
 			/>
 
 			<button
@@ -31,18 +38,28 @@ export default function Track() {
 				{replayButton}
 			</button>
 
-			<div>
-				{isTrackMuted ? volumeOffButton : volumeUpButton}
-				<input
-					type='range'
-					min='0'
-					max='1'
-					step='0.01'
-					defaultValue='1'
-					onChange={handleVolumeChange}
-				>
-				</input>
-			</div>
+			<p>{handleTrackTime(trackCurrentTime)}</p>
+
+			<input
+				type='range'
+				min='0'
+				max={trackDuration || 0}
+				step='0.01'
+				value={trackCurrentTime || 0}
+				onChange={handleTimeSeek}
+			/>
+
+			<p>{trackPreviewDetails ? handleTrackTime(trackDuration) : '0:00'}</p>
+
+			{isTrackMuted ? volumeOffButton : volumeUpButton}
+			<input
+				type='range'
+				min='0'
+				max='1'
+				step='0.01'
+				defaultValue='1'
+				onChange={handleVolumeChange}
+			/>
 
 		</div>
 	)
