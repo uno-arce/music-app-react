baa0d6aa54bb69f26b5694bf526f44c668ac8481
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-12-14 22:42:01 +0800

    Added functions for handling next and back button of collection

--- src/hooks/collectionHooks.js
+++ src/hooks/collectionHooks.js
@@ -3,6 +3,8 @@ import useComponentStore from '../stores/componentStore'
 const useCollection = () => {
 	const componentStore = useComponentStore()
 
+	let collectionSelectedGroup = componentStore.collectionSelectedGroup
+
 	const handleOpenCollectionView = (item, index, isSelectable) => {
 		if(!isSelectable) {
 			return
@@ -13,8 +15,29 @@ const useCollection = () => {
 		componentStore.setIsCollectionOpen(true)
 	}
 
+	const handlePreviousCollectionGroup = () => {
+		if(collectionSelectedGroup == 1) {
+			return
+		}
+
+		componentStore.setCollectionSelectedGroup(collectionSelectedGroup - 1)
+		componentStore.setCollectionSelectedIndex(0)
+	}
+
+	const handleNextCollectionGroup = (collection) => {
+		const collectionGroupLength = Object.keys(collection).length
+		if(collectionSelectedGroup == collectionGroupLength) {
+			return
+		}
+
+		componentStore.setCollectionSelectedGroup(collectionSelectedGroup + 1)
+		componentStore.setCollectionSelectedIndex(0)
+	}
+
 	return {
 		handleOpenCollectionView,
+		handlePreviousCollectionGroup,
+		handleNextCollectionGroup,
 		isCollectionOpen: componentStore.isCollectionOpen,
 		collectionSelectedIndex: componentStore.collectionSelectedIndex
 	}
