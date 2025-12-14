e235e5f2ac72e4a78750745ad4b844e9ffcbcec0
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2025-12-14 22:33:49 +0800

    Modified button to accept children

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { buttonStyle } from '../styles/style'
 
-export default function Button({name, id, call, isDisabled}) {
+export default function Button({name, id, call, isDisabled, children}) {
 	const buttonClasses = buttonStyle(isDisabled)
 
 	return(
 		<div>
-			<button className={buttonClasses} type='submit' disabled={isDisabled} onClick={call}>{name}</button>
+			<button className={buttonClasses} type='submit' disabled={isDisabled} onClick={call}>{name}{children}</button>
 		</div>
 	)
 }
