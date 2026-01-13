253c435990592c162d60e627efba9f44889ee49e
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-01-13 20:25:33 +0800

    Modified form structure and styling

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import useForm from '../hooks/formHooks'
 
-export default function Form({ inputs, call, isDisabled, isStepForm, formValidator, children }) {
+export default function Form({ inputs, call, isDisabled, isStepForm, formValidator, structure, children }) {
 	const { currentFormStep, handleFormSubmit } = useForm()
 
 	const formInputs = inputs.map(field => {
 		return (
 			(currentFormStep == field.name || !isStepForm) &&
-			<div key={field.name}>
-				<p htmlFor={field.name}>{field.name}</p>
+			<div
+				className='flex flex-col gap-2'
+				key={field.name}
+			>
+				<label>{field.name}</label>
 				<input
 				name={field.name}
 				type={field.type || 'text'}
@@ -18,6 +21,7 @@ export default function Form({ inputs, call, isDisabled, isStepForm, formValidat
 				}}
 				value={field.value}
 				disabled={isDisabled}
+				className={`input`}
 				/>
 				{formValidator && formValidator(field.name)}
 			</div>
@@ -25,7 +29,10 @@ export default function Form({ inputs, call, isDisabled, isStepForm, formValidat
 	})
 
 	return (
-		<form onSubmit={event => handleFormSubmit(event, call)}>
+		<form 
+			onSubmit={event => handleFormSubmit(event, call)}
+			className={structure}
+		>
 			{formInputs}
 			{children}
 		</form>
