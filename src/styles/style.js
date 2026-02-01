5eb4373c47f69aad69e5c1c6834bec131fd63efa
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-01 18:12:49 +0800

    Added dark theme variables

--- src/styles/style.js
+++ src/styles/style.js
@@ -41,7 +41,7 @@ export const ratingStyle = (clickedIndex) => {
 	const ratingSymbolThird= `peer/third ${clickedIndex >= 3 ? 'fill-accent' : 'fill-accent-light'} hover:fill-accent peer-hover/fifth:fill-accent peer-hover/fourth:fill-accent transition-all`
 	const ratingSymbolSecond= `peer/second ${clickedIndex >= 2 ? 'fill-accent' : 'fill-accent-light'} hover:fill-accent peer-hover/fifth:fill-accent peer-hover/fourth:fill-accent peer-hover/third:fill-accent transition-all`
 	const ratingSymbolFirst= `peer/first ${clickedIndex >= 1 ? 'fill-accent' : 'fill-accent-light'} hover:fill-accent peer-hover/fifth:fill-accent peer-hover/fourth:fill-accent peer-hover/third:fill-accent peer-hover/second:fill-accent transition-all`
-	const ratingGroup = 'flex flex-col items-start bg-[url(src/assets/clean-gray-paper.png)] p-2'
+	const ratingGroup = 'flex flex-col items-start bg-[image:var(--background-main)] p-2'
 	const ratingTitle = 'text-2xl text-left text-pretty grow'
 	const ratingSubtitle = 'text-sm text-base-light'
 	const ratingDefault = 'flex flex-row-reverse'
