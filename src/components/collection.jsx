6074d6befbb35242f6f71ea017fe758612fb9666
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-03 19:06:03 +0800

    Modified collection as motion component

--- src/components/collection.jsx
+++ src/components/collection.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import useCollection from '../hooks/collectionHooks'
 
-export default function Collection({ items, isSelectable, openCollection, isOpen, renderItem, structure, children }) {
+export default function Collection({ items, isSelectable, openCollection, isOpen, renderItem, structure, itemVariants, children, ...motionProps }) {
 
 	const { collectionSelectedIndex } = useCollection()
 
 	const dataCollection = items.map((item, index) => {
 		return(
-			<div 
+			<motion.div 
 				className={collectionSelectedIndex === index ? 'collection-item-active' : null }
 				key={index}
 				onClick={() => openCollection(item, index, isSelectable)} 
+				variants={itemVariants}
 			>
 				{renderItem(item, index)}
-			</div>
+			</motion.div>
 		)
 	})
 	return(
-		<div className={structure}>
+		<motion.div 
+			className={structure} 
+			{...motionProps}>
 			{dataCollection}
 			{isOpen && children}
-		</div>
+		</motion.div>
 	)
 }
