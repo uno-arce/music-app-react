ca4a36db4aadb929b9c60d329578076a862646b0
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-03 19:07:30 +0800

    Added various item entrances

--- src/styles/motion.js
+++ src/styles/motion.js
@@ -31,6 +31,24 @@ export const slideInRight = {
 	}
 }
 
+export const slideInTop = {
+	hidden: { opacity: 0, y: -20 },
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: { type: 'spring', stiffness: 260, damping: 20 }
+	}
+}
+
+export const slideInBottom = {
+	hidden: { opacity: 0, y: 20 },
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: { type: 'spring', stiffness: 260, damping: 20 }
+	}
+}
+
 export const fadeInLeft = {
 	hidden: { opacity: 0, x: -20 },
 	show: {
@@ -45,6 +63,24 @@ export const fadeInRight = {
 	show: {
 		opacity: 1,
 		x: 0,
+		transition: { duration: 0.5 }
+	 }
+}
+
+export const fadeInTop = {
+	hidden: { opacity: 0, y: -20 },
+	show: {
+		opacity: 1,
+		y: 0,
+		transition: { duration: 0.5 }
+	 }
+}
+
+export const fadeInBottom = {
+	hidden: { opacity: 0, y: 20 },
+	show: {
+		opacity: 1,
+		y: 0,
 		transition: { duration: 0.5 }
 	 }
 }
