669d07a8a3ba8b7377e2b0f82f2d93ea21a93ae7
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-04 19:34:17 +0800

    Modified popover as motion component

--- src/components/popover.jsx
+++ src/components/popover.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AnimatePresence } from 'framer-motion'
 
 export default function Popover({ renderPopover, close, children }) {
 	return(
-		<>
+		<AnimatePresence>
 			{renderPopover(children)}
-		</>
+		</AnimatePresence>
 	)
 }
