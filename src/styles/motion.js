166ef41154bf6f3c5bcd2ac769bf0a935418e739
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-04 19:33:26 +0800

    Added motion item entrances

--- src/styles/motion.js
+++ src/styles/motion.js
@@ -49,6 +49,12 @@ export const slideInBottom = {
 	}
 }
 
+export const fadeOut = {
+	hidden: { opacity: 0 },
+	show: { opacity: 1 },
+	remove: { opacity: 0 }
+}
+
 export const fadeInLeft = {
 	hidden: { opacity: 0, x: -20 },
 	show: {
@@ -83,6 +89,24 @@ export const fadeInBottom = {
 		y: 0,
 		transition: { duration: 0.5 }
 	 }
+}
+
+export const scaleIn = {
+	hidden: { opacity: 0, scale: 0.5 },
+	show: {
+		opacity: 1,
+		scale: 1,
+		transition: { 
+			type: 'spring', 
+			stiffness: 300,
+			damping: 25
+		}
+	},
+	exit: {
+		opacity: 0,
+		scale: 0.5,
+		transition: { duration: 0.5 }
+	}
 }
 
 // 3. Hover & Tap Micro-interaction
