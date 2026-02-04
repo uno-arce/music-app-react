729dbcaa9ad4dbe8503f718613c7c9a9aea7acf1
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-04 19:33:49 +0800

    Modified placeholder logic and as motion component

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { fadeOut } from '../styles/motion'
 
-export default function Placeholder({ isLoading, children }) {
-
-	if(isLoading) {
-		return <div className='flex animate-pulse'></div>
-	}
-
-	return children
+export default function Placeholder({ isLoading, skeletonNumbers, structure, children }) {
+	const skeleton = [...Array(skeletonNumbers)].map((_, index) => (
+		<div key={index} className={`${structure.skeleton} animate-pulse`}/>
+	))
+	return (
+		<AnimatePresence mode='wait'>
+			{isLoading ? (
+				<motion.div
+					key='skeleton'
+					variants={fadeOut}
+					initial='hidden'
+					animate='show'
+					exit='remove'
+					className={structure.parent}>
+					{skeleton}
+				</motion.div>
+			) : (
+				children
+			)}
+		</AnimatePresence>
+	)
 }
