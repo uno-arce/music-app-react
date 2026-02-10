9469b36e9b7a383dca5efa29280717b1b68818c8
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-10 18:13:55 +0800

    Modified sidebar as motion component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import useSidebar from '../hooks/sidebarHooks'
+import { motion, AnimatePresence } from 'framer-motion'
+import { fadeOut } from '../styles/motion'
 
-export default function Sidebar ({structure, children}) {
+export default function Sidebar ({structure, children, ...motionProps }) {
 	const { handleCloseSidebarView, isSidebarOpen } = useSidebar()
 
 	return (
-		<>
+		<AnimatePresence>
 			{isSidebarOpen && (
 				<>
-					<div
-						className='xl:hidden fixed inset-0 backdrop-blur-xs z-4'
+					<motion.div
+						className='fixed inset-0 backdrop-blur-xs z-4'
 						onClick={handleCloseSidebarView}
+						variants={fadeOut}
+						initial='hidden'
+						animate='show'
+						exit='remove'
 					/>
-					<div className={structure}>
+					<motion.div className={structure} {...motionProps}>
 						{children}
-					</div>
+					</motion.div>
 				</>
 			)}
-		</>
+		</AnimatePresence>
 	)
 }
