aff84cfa6e1ba5ab7635a1bf733bf1ffe78cfd2d
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-19 19:39:30 +0800

    Added useShallow and custom hooks

--- src/stores/componentStore.js
+++ src/stores/componentStore.js
@@ -1,62 +1,79 @@
 import { create } from 'zustand'
+import { useShallow } from 'zustand/react/shallow'
 
 const useComponentStore = create((set) => ({
+	// Alert
+	alertMessage: false,
+	alertMessage: null,
+	alertMessage: null,
+	actionsAlert: {
+		setIsAlertOpen: (isOpen) => set({ isAlertOpen: isOpen }),
+		setAlertStatus: (status) => set({ alertStatus: status }),
+		setAlertMessage: (alertMessage) => set({ alertMessage: alertMessage }),
+	}
+
 	// Collection
 	collectionItem: null,
 	collectionSelectedGroup: 1,
 	collectionSelectedIndex: 0,
 	isCollectionOpen: false,
-	setCollectionItem: (collectionItem) => set({ collectionItem: collectionItem }),
-	setCollectionSelectedGroup: (selectedGroup) => set({collectionSelectedGroup: selectedGroup}),
-	setCollectionSelectedIndex: (selectedIndex) => set({ collectionSelectedIndex: selectedIndex}),
-	setIsCollectionOpen: (isOpen) => set({ isCollectionOpen: isOpen }),
+	actionsCollection: {
+		setCollectionItem: (collectionItem) => set({ collectionItem: collectionItem }),
+		setCollectionSelectedGroup: (selectedGroup) => set({collectionSelectedGroup: selectedGroup}),
+		setCollectionSelectedIndex: (selectedIndex) => set({ collectionSelectedIndex: selectedIndex}),
+		setIsCollectionOpen: (isOpen) => set({ isCollectionOpen: isOpen }),
+	}
+
+	// Form
+	currentFormStep: 'Username',
+	actionsForm: {
+		setCurrentFormStep: (currentFormStep) => set({ currentFormStep: currentFormStep }),
+	}
+
+	// Menu
+	selectedMenuCategory: 'recentlyPlayed',
+	selectedMenuLabel: 'Recently Played',
+	actionsMenu: {
+		setSelectedMenuCategory: (category) => set({ selectedMenuCategory: category }),
+		setSelectedMenuLabel: (label) => set({  selectedMenuLabel: label }),
+	}
 
 	// Popover
 	popoverItem: null,
 	isPopoverOpen: false,
-	setPopoverItem: (popoverItem) => set({ popoverItem: popoverItem }),
-	setIsPopoverOpen: (isOpen) => set({ isPopoverOpen: isOpen }),
+	actionsPopover: {
+		setPopoverItem: (popoverItem) => set({ popoverItem: popoverItem }),
+		setIsPopoverOpen: (isOpen) => set({ isPopoverOpen: isOpen }),
+	}
 
 	//Rating
 	hoveredIndex: 0,
 	clickedIndex: 0,
 	lastClickedIndex: 0,
-	setHoveredIndex: (hoveredIndex) => set({ hoveredIndex: hoveredIndex }),
-	setClickedIndex: (clickedIndex) => set({ clickedIndex: clickedIndex }),
-	setLastClickedIndex: (lastClickedIndex) => set({ lastClickedIndex: lastClickedIndex }),
-	resetRatingState: () => {
-		set({
-			hoveredIndex: -1,
-			clickedIndex: -1,
-			lastClickedIndex: -1
-		})
-	},
-
-	// Alert
-	isAlertOpen: false,
-	alertStatus: null,
-	alertMessage: null,
-	setIsAlertOpen: (isOpen) => set({ isAlertOpen: isOpen }),
-	setAlertStatus: (status) => set({ alertStatus: status }),
-	setAlertMessage: (alertMessage) => set({ alertMessage: alertMessage }),
-
-	// Form
-	currentFormStep: 'Username',
-	setCurrentFormStep: (currentFormStep) => set({ currentFormStep: currentFormStep }),
-
-	// Menu
-	selectedMenuCategory: 'recentlyPlayed',
-	selectedMenuLabel: 'Recently Played',
-	setSelectedMenuCategory: (category) => set({ selectedMenuCategory: category }),
-	setSelectedMenuLabel: (label) => set({  selectedMenuLabel: label }),
+	actionsRating: {
+		setHoveredIndex: (hoveredIndex) => set({ hoveredIndex: hoveredIndex }),
+		setClickedIndex: (clickedIndex) => set({ clickedIndex: clickedIndex }),
+		setLastClickedIndex: (lastClickedIndex) => set({ lastClickedIndex: lastClickedIndex }),
+		resetRatingState: () => {
+			set({
+				hoveredIndex: -1,
+				clickedIndex: -1,
+				lastClickedIndex: -1
+			})
+		},
+	}
 
 	// Sidebar
 	isSidebarOpen: false,
-	setIsSidebarOpen: (isOpen) => set({ isSidebarOpen: isOpen }),
+	actionsSidebar: {
+		setIsSidebarOpen: (isOpen) => set({ isSidebarOpen: isOpen }),
+	}
 
 	// ThemeToggle
 	isDarkMode: localStorage.getItem('theme') === 'dark',
-	setIsDarkMode: (mode) => set({ isDarkMode: mode }),
+	actionsThemeToggle: {
+		setIsDarkMode: (mode) => set({ isDarkMode: mode }),
+	}
 
 	// Track
 	trackPreviewDetails: null,
@@ -66,24 +83,116 @@ const useComponentStore = create((set) => ({
 	isTrackPlaying: false,
 	isTrackPaused: false,
 	isTrackMuted: false,
-	setTrackPreviewDetails: (preview) => set({ trackPreviewDetails: preview }),
-	setTrackCurrentTime: (currentTime) => set({ trackCurrentTime: currentTime }),
-	setTrackDuration: (duration) => set({ trackDuration: duration }),
-	setIsTrackOpen: (isOpen) => set({ isTrackOpen: isOpen }),
-	setIsTrackPlaying: (isTrackPlaying) => set({ isTrackPlaying: isTrackPlaying }),
-	setIsTrackPaused: (isPaused) => set({ isTrackPaused: isPaused }),
-	setIsTrackMuted: (isMuted) => set({ isTrackMuted: isMuted }),
-	resetTrackState: () => {
-		set({
-			trackPreviewDetails: null,
-			trackCurrentTime: 0,
-			trackDuration: null,
-			isTrackPlaying: false,
-			isTrackPaused: false,
-			isTrackEnded: false,
-			isTrackMuted: false
-		})
+	actionsTrack: {
+		setTrackPreviewDetails: (preview) => set({ trackPreviewDetails: preview }),
+		setTrackCurrentTime: (currentTime) => set({ trackCurrentTime: currentTime }),
+		setTrackDuration: (duration) => set({ trackDuration: duration }),
+		setIsTrackOpen: (isOpen) => set({ isTrackOpen: isOpen }),
+		setIsTrackPlaying: (isTrackPlaying) => set({ isTrackPlaying: isTrackPlaying }),
+		setIsTrackPaused: (isPaused) => set({ isTrackPaused: isPaused }),
+		setIsTrackMuted: (isMuted) => set({ isTrackMuted: isMuted }),
+		resetTrackState: () => {
+			set({
+				trackPreviewDetails: null,
+				trackCurrentTime: 0,
+				trackDuration: null,
+				isTrackPlaying: false,
+				isTrackPaused: false,
+				isTrackEnded: false,
+				isTrackMuted: false
+			})
+		}
 	}
 }))
 
 export default useComponentStore
+
+
+// Custom Hooks utilizing useShallow to prevent unnecessary re-renders
+
+// Alert
+export const useAlertData = () => useComponentStore(useShallow((state) => {
+	return {
+		isAlertOpen: state.isAlertOpen,
+		alertStatus: state.alertStatus,
+		alertMessage: state.alertMessage
+	}
+}))
+export const useAlertActions = () => useComponentStore((state) => state.actionsAlert)
+
+// Collection
+export const useCollectionData = () => useComponentStore(useShallow((state) => {
+	return {
+		collectionItem: state.collectionItem,
+		collectionSelectedGroup: state.collectionSelectedGroup,
+		collectionSelectedIndex: state.collectionSelectedIndex,
+		isCollectionOpen: state.isCollectionOpen
+	}
+}))
+export const useCollectionActions = () => useComponentStore((state) => state.actionsCollection)
+
+// Form
+export const useFormData = () => useComponentStore(useShallow((state) => {
+	return {
+		currentFormStep: state.currentFormStep
+	}
+}))
+export const useFormActions = () => useComponentStore((state) => state.actionsForm)
+
+// Menu 
+export const useMenuData = () => useComponentStore(useShallow((state) => {
+	return {
+		selectedMenuCategory: state.selectedMenuCategory,
+		selectedMenuLabel state.selectedMenuLabel
+	}
+}))
+export const useMenuActions = () => useComponentStore((state) => state.actionsMenu)
+
+// Popover
+export const usePopoverData = () => useComponentStore(useShallow((state) => {
+	return {
+		popoverItem: state.popoverItem,
+		isPopoverOpen state.isPopoverOpen
+	}
+}))
+export const usePopoverActions = () => useComponentStore((state) => state.actionsPopover)
+
+// Rating
+export const useRatingData = () => useComponentStore(useShallow((state) => {
+	return {
+		hoveredIndex: state.hoveredIndex,
+		clickedIndex: state.clickedIndex,
+		lastClickedIndex state.lastClickedIndex
+	}
+}))
+export const useRatingActions = () => useComponentStore((state) => state.actionsRating)
+
+// Sidebar 
+export const useSidebarData = () => useComponentStore(useShallow((state) => {
+	return {
+		isSidebarOpen: state.isSidebarOpen
+	}
+}))
+export const useSidebarActions = () => useComponentStore((state) => state.actionsSidebar)
+
+// ThemeToggle
+export const useThemeToggleData = () => useComponentStore(useShallow((state) => {
+	return {
+		isDarkMode: state.isDarkMode
+	}
+}))
+export const useThemeToggleActions = () => useComponentStore((state) => state.actionsThemeToggle)
+
+// Track
+export const useTrackData = () => useComponentStore(useShallow((state) => {
+	return {
+		trackPreviewDetails: state.trackPreviewDetails,
+		trackCurrentTime: state.trackCurrentTime,
+		trackDuration: state.trackDuration,
+		isTrackOpen: state.isTrackOpen,
+		isTrackPlaying: state.isTrackPlaying,
+		isTrackPaused: state.isTrackPaused,
+		isTrackMuted: state.isTrackMuted
+	}
+}))
+export const useTrackActions = () => useComponentStore((state) => state.actionsTrack)
--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { useShallow } from 'zustand/react/shallow'
 
 const useUserAuthStore = create((set) => ({
 	username: '',
@@ -18,48 +19,83 @@ const useUserAuthStore = create((set) => ({
 	isPasswordLengthCorrect: false,
 	isPasswordVisible: false,
 
-	setUsername: (username) => set({ username: username }),
-	setEmail: (email) => set({ email: email }),
-	setPassword: (password) => set({ password: password }),
-	setIsFormDisabled: (form) => set({ isFormDisabled: form }),
-	setIsAuthenticated: (authenticated) => set({ isAuthenticated: authenticated }),
-	setIsLoading: (loading) => set({ isLoading: loading }),
+	actions: {
+		setUsername: (username) => set({ username: username }),
+		setEmail: (email) => set({ email: email }),
+		setPassword: (password) => set({ password: password }),
+		setIsFormDisabled: (form) => set({ isFormDisabled: form }),
+		setIsAuthenticated: (authenticated) => set({ isAuthenticated: authenticated }),
+		setIsLoading: (loading) => set({ isLoading: loading }),
 
-	setIsUsernameLengthCorrect: (isLengthCorrect) => set({ isUsernameLengthCorrect: isLengthCorrect }),
-	setIsUsernameCharactersCorrect: (isCharactersCorrect) => set({ isUsernameCharactersCorrect: isCharactersCorrect }),
-	setIsEmailFormatCorrect: (isEmailFormatCorrect) => set({ isEmailFormatCorrect: isEmailFormatCorrect }),
-	setIsEmailAvailable: (isEmailAvailable) => set({ isEmailAvailable: isEmailAvailable }),
-	setIsPasswordCharactersCorrect: (isCharactersCorrect) => set({ isPasswordCharactersCorrect: isCharactersCorrect }),
-	setIsPasswordTextCaseCorrect: (isTextCaseCorrect) => set({ isPasswordTextCaseCorrect: isTextCaseCorrect }),
-	setIsPasswordLengthCorrect: (isLengthCorrect) => set({ isPasswordLengthCorrect: isLengthCorrect }),
-	setIsPasswordVisible: (isVisible) => set({ isPasswordVisible: isVisible }),
+		setIsUsernameLengthCorrect: (isLengthCorrect) => set({ isUsernameLengthCorrect: isLengthCorrect }),
+		setIsUsernameCharactersCorrect: (isCharactersCorrect) => set({ isUsernameCharactersCorrect: isCharactersCorrect }),
+		setIsEmailFormatCorrect: (isEmailFormatCorrect) => set({ isEmailFormatCorrect: isEmailFormatCorrect }),
+		setIsEmailAvailable: (isEmailAvailable) => set({ isEmailAvailable: isEmailAvailable }),
+		setIsPasswordCharactersCorrect: (isCharactersCorrect) => set({ isPasswordCharactersCorrect: isCharactersCorrect }),
+		setIsPasswordTextCaseCorrect: (isTextCaseCorrect) => set({ isPasswordTextCaseCorrect: isTextCaseCorrect }),
+		setIsPasswordLengthCorrect: (isLengthCorrect) => set({ isPasswordLengthCorrect: isLengthCorrect }),
+		setIsPasswordVisible: (isVisible) => set({ isPasswordVisible: isVisible }),
 
 
-	resetUserAuthState: () => {
-		set({
-			username: '',
-			email: '',
-			password: '',
-			isFormDisabled: false
-		})
-	},
+		resetUserAuthState: () => {
+			set({
+				username: '',
+				email: '',
+				password: '',
+				isFormDisabled: false
+			})
+		},
 
-	resetUserRegistrationState: () => {
-		set({
-			username: '',
-			email: '',
-			password: '',
-			isFormDisabled: false,
-			isUsernameLengthCorrect: false,
-			isUsernameCharactersCorrect: false,
-			isEmailFormatCorrect: false,
-			isEmailAvailable: false,
-			isPasswordCharactersCorrect: false,
-			isPasswordTextCaseCorrect: false,
-			isPasswordLengthCorrect: false,
-			isPasswordVisible: false,
-		})
+		resetUserRegistrationState: () => {
+			set({
+				username: '',
+				email: '',
+				password: '',
+				isFormDisabled: false,
+				isUsernameLengthCorrect: false,
+				isUsernameCharactersCorrect: false,
+				isEmailFormatCorrect: false,
+				isEmailAvailable: false,
+				isPasswordCharactersCorrect: false,
+				isPasswordTextCaseCorrect: false,
+				isPasswordLengthCorrect: false,
+				isPasswordVisible: false,
+			})
+		}
 	}
 }))
+
+export const useUserAuthData = () => useUserAuthStore(useShallow((state) => {
+	const isUsernameIncorrect = !state.isUsernameLengthCorrect || !state.isUsernameCharactersCorrect
+
+	const isEmailIncorrect = !state.isEmailFormatCorrect || !state.isEmailAvailable
+
+	const isPasswordIncorrect = !state.isPasswordCharactersCorrect || !state.isPasswordTextCaseCorrect && !state.isPasswordLengthCorrect
+	const isLoginButtonDisabled = !state.email || !state.password
+	const isRegisterButtonDisabled = state.isUsernameIncorrect || state.isEmailIncorrect || state.isPasswordIncorrect || !state.username || !state.email || !state.password
+	return {
+		username: state.username,
+		email: state.email,
+		password: state.password,
+		isAuthenticated: state.isAuthenticated,
+		isFormDisabled: state.isFormDisabled,
+		isLoading: state.isLoading,
+		isUsernameLengthCorrect: state.isUsernameLengthCorrect,
+		isUsernameCharactersCorrect: state.isUsernameCharactersCorrect,
+		isEmailFormatCorrect: state.isEmailFormatCorrect,
+		isEmailAvailable: state.isEmailAvailable,
+		isPasswordCharactersCorrect: state.isPasswordCharactersCorrect,
+		isPasswordTextCaseCorrect: state.isPasswordTextCaseCorrect,
+		isPasswordLengthCorrect: state.isPasswordLengthCorrect,
+		isPasswordVisible: state.isPasswordVisible,
+		isUsernameIncorrect,
+		isEmailIncorrect,
+		isPasswordIncorrect,
+		isLoginButtonDisabled,
+		isRegisterButtonDisabled
+	}
+}))
+
+export const useUserAuthActions = () => useUserAuthStore((state) => state.actions)
 
 export default useUserAuthStore
