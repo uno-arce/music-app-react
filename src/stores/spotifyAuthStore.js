1157c5682b99434a8307df9f0ee28c40b7c0cd47
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-22 20:40:02 +0800

    Modified spotify auth store to use useShallow

--- src/stores/spotifyAuthStore.js
+++ src/stores/spotifyAuthStore.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { useShallow } from 'zustand/react/shallow'
 
 const useSpotifyAuthStore = create((set) => ({
 	isAuthorized: false,
@@ -6,9 +7,22 @@ const useSpotifyAuthStore = create((set) => ({
 	isAuthLoading: true,
 
 
-	setIsAuthorized: (authorization) => set({ isAuthorized: authorization }),
-	setIsTokenExpired: (expiration) => set({ isTokenExpired: expiration }),
-	setIsAuthLoading: (loading) => set({ isAuthLoading: loading })
+	actions: {
+		setIsAuthorized: (authorization) => set({ isAuthorized: authorization }),
+		setIsTokenExpired: (expiration) => set({ isTokenExpired: expiration }),
+		setIsAuthLoading: (loading) => set({ isAuthLoading: loading })
+	}
 }))
 
 export default useSpotifyAuthStore
+
+
+export const useSpotifyAuthData = () => useSpotifyAuthStore(useShallow((state) => {
+	return {
+		isAuthorized: state.isAuthorized,
+		isTokenExpired: state.isTokenExpired,
+		isAuthLoading: state.isAuthLoading
+	}
+}))
+
+export const useSpotifyAuthActions = () => useSpotifyAuthStore((state) => state.actions)
