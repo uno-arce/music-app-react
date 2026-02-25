278a9a7168d0362561436d1740b1424e442bb04a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-02-25 21:25:32 +0800

    Added loading path for alert status

--- src/styles/style.js
+++ src/styles/style.js
@@ -62,7 +62,7 @@ export const ratingStyle = (clickedIndex) => {
 export const alertStyle = (status) => {
 	const alertPosition = 'fixed z-10 inset-0 flex flex-col justify-center items-center'
 	const alertGroup = 'absolute top-0 flex min-h-fit min-w-fit p-4 mt-4 gap-4 bg-base rounded-sm'
-	const alertIcon = 'fill-base-light'
+	const alertIcon = `fill-base-light ${status === 'loading' ? 'animate-spin' : null}`
 	const alertDescription = 'text-left text-base-light'
 	return {
 		alertPosition,
